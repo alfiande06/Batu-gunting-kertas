666a2e70dba11a6ea39979eb5ea81e7e041fa2b5
alfiande06 <alfian061295@gmail.com>
2024-05-04 20:45:00 +0700

    Perubahan MVC 4 mei 2024

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware untuk menguraikan body dari request HTTP
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Middleware untuk penyediaan file statis dari folder 'assets'
+app.use('/assets', express.static(__dirname + '/assets'));
+
+// Middleware untuk penyediaan file statis dari folder 'views'
+app.use('/views', express.static(__dirname + '/views'));
+
+// Middleware untuk penyediaan file statis dari folder 'controller'
+app.use('/controllers', express.static(__dirname + '/controllers'));
+
+// Set view engine ke EJS
+app.set('view engine', 'ejs');
+
+// Routes
+app.get('/index.html', function (req, res){
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+// Port binding
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
